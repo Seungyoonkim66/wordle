08c1e8ede12a7e3d6be756bcaf47a75441f9529f
Seungyoonkim66 <tmddbs1998@naver.com>
2022-05-15 16:19:30 +0900

    gitignore 적용

--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,25 @@ const answer = "hello";
 const wordCnt = 5;
 let activeRound = 1;
 const getBoxContainerId = (round) => "round-" + round;
-const getInputBoxId = (index) => "box-" + index;
-const checkAnswer = (answer) => {
-    console.log(answer);
+const getInputBoxId = (round, index) => "round-" + round + "box-" + index;
+const checkAnswer = (round) => {
+    const roundContainer = document.getElementById(getBoxContainerId(round));
+    roundContainer === null || roundContainer === void 0 ? void 0 : roundContainer.childNodes.forEach((inputBox, index) => {
+        const answerBox = inputBox;
+        const currentAlphabet = answerBox.value;
+        /* 위치는 다르지만 정답에 포함된 알파벳인 경우 */
+        if (answer.includes(currentAlphabet)) {
+            answerBox.className = "input-box correct";
+        }
+        /* 위치와 알파벳이 모두 일치하는 경우 */
+        if (answer[index] == currentAlphabet) {
+            answerBox.className = "input-box correct-correct";
+        }
+        /* 위치, 알파벳 모두 틀린 경우 */
+        else {
+            answerBox.className = "input-box wrong";
+        }
+    });
 };
 const addRound = (playBoard, round) => {
     if (playBoard != null) {
@@ -15,19 +31,27 @@ const addRound = (playBoard, round) => {
         playBoard === null || playBoard === void 0 ? void 0 : playBoard.append(boxContainer);
         for (let i = 1; i <= wordCnt; i++) {
             const inputBox = document.createElement("input");
-            inputBox.id = getInputBoxId(i);
+            inputBox.id = getInputBoxId(round, i);
             inputBox.maxLength = 1;
             inputBox.className = "input-box basic";
             inputBox === null || inputBox === void 0 ? void 0 : inputBox.addEventListener("keyup", (e) => {
                 const box = e.target;
                 const answer = box === null || box === void 0 ? void 0 : box.value;
+                /* 한글자 입력후 옆 input으로 포커스 이동 + 마지막엔 채점 메소드 호출 */
                 if (answer != null && answer.length == inputBox.maxLength) {
                     const nextBox = box === null || box === void 0 ? void 0 : box.nextElementSibling;
                     if (nextBox != null) {
                         nextBox.focus();
                     }
                     else {
-                        console.log("정답 제출");
+                        checkAnswer(round);
+                    }
+                }
+                /* 백스페이스 누르면 뒤 input으로 포커스 이동 */
+                if (e.key == "Backspace") {
+                    const prevBox = box === null || box === void 0 ? void 0 : box.previousElementSibling;
+                    if (prevBox != null) {
+                        prevBox.focus();
                     }
                 }
             });
--- scripts/index.ts
+++ scripts/index.ts
@@ -3,10 +3,26 @@ const wordCnt:number = 5;
 let activeRound:number = 1;
 
 const getBoxContainerId = (round:number):string => "round-" + round; 
-const getInputBoxId = (index:number):string => "box-" + index;
+const getInputBoxId = (round:number, index:number):string => "round-" + round + "box-" + index;
 
-const checkAnswer = (answer:string) => {
-    console.log(answer)
+const checkAnswer = (round:number) => {
+    const roundContainer = document.getElementById(getBoxContainerId(round));
+    roundContainer?.childNodes.forEach((inputBox, index)=> {
+        const answerBox = inputBox as HTMLInputElement;
+        const currentAlphabet = answerBox.value;
+        /* 위치는 다르지만 정답에 포함된 알파벳인 경우 */
+        if(answer.includes(currentAlphabet)){ 
+            answerBox.className = "input-box correct"
+        }
+        /* 위치와 알파벳이 모두 일치하는 경우 */
+        if (answer[index] == currentAlphabet){
+            answerBox.className = "input-box correct-correct"
+        }
+        /* 위치, 알파벳 모두 틀린 경우 */
+        else {
+            answerBox.className = "input-box wrong"
+        }
+    });
 }
 
 const addRound = (playBoard:HTMLElement | null, round:number):void => {
@@ -17,20 +33,27 @@ const addRound = (playBoard:HTMLElement | null, round:number):void => {
         playBoard?.append(boxContainer);
         for(let i:number = 1; i<=wordCnt; i++){
             const inputBox:HTMLInputElement = document.createElement("input");
-            inputBox.id = getInputBoxId(i);
+            inputBox.id = getInputBoxId(round, i);
             inputBox.maxLength = 1;
             inputBox.className = "input-box basic";
-            inputBox?.addEventListener("keyup", (e) => {
+            inputBox?.addEventListener("keyup", (e:KeyboardEvent) => {
                 const box = e.target as HTMLInputElement | null;
                 const answer = box?.value;
+                /* 한글자 입력후 옆 input으로 포커스 이동 + 마지막엔 채점 메소드 호출 */
                 if(answer != null && answer.length == inputBox.maxLength){
                     const nextBox= box?.nextElementSibling as HTMLElement;
                     if(nextBox != null){
                         nextBox.focus();
                     } else {
-                        console.log("정답 제출")
+                        checkAnswer(round);
                     }
-                    
+                }
+                /* 백스페이스 누르면 뒤 input으로 포커스 이동 */
+                if(e.key == "Backspace"){
+                    const prevBox = box?.previousElementSibling as HTMLElement;
+                    if(prevBox != null){
+                        prevBox.focus();
+                    }
                 }
             });
             boxContainer.append(inputBox);
